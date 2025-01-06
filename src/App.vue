ba45d88dfb7e8c34d04997c89165f0cf82f15fe0
ChatGIS <ChatGIS@outlook.com>
2025-01-07 00:04:54 +0800

    v1.1.0 add vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,18 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue + Test " />
+  <h1>Hello App!</h1>
+  <p>
+    <strong>Current route path:</strong> {{ route.fullPath }}
+  </p>
+  <nav>
+    <RouterLink to="/">Go to Home</RouterLink>
+    <RouterLink to="/about">Go to About</RouterLink>
+  </nav>
+  <main>
+    <RouterView />
+  </main>
 </template>
+<script lang="js" setup>
+import { useRoute } from 'vue-router';
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+const route =  useRoute()
+</script>
